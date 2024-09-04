f32ae64614d700c54d3dffa456012d19199461e3
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-09-04 17:15:46 -0300

    feat: Create API routes
    Create the API routes for:
    
    - `/especialidade`
    - `/unidades?cep=&eps=`
    - `/detalhes?cnes=`
    
    The routes aren't functional yet, just implemented

--- src/app.py
+++ src/app.py
@@ -5,4 +5,16 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Não há nada aqui, apenas um sanity check!"}
+
+@app.get("/especialidades")
+async def especialidades():
+    return {"message": "Não implementado ainda!"}
+
+@app.get("/unidades")
+async def unidades(cep: str, esp: str):
+    return {"message": f"Não implementado ainda! Unidades próximas ao CEP {cep} com especialidade {esp}"}
+
+@app.get("/detalhes")
+async def detalhes(cnes: int):
+    return {"message": f"Não implementado ainda! Detalhes do CNES {cnes}"}
