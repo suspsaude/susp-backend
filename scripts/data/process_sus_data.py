bce9c67e253fc6e05b6d57ba6583a54a71d0dd38
Joao Paulo Pereira da Silva <jppaulo11@usp.br>
2024-09-21 19:52:07 -0300

    style: Apply PEP-8 on scripts

--- scripts/data/process_sus_data.py
+++ scripts/data/process_sus_data.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
-"""Script to process the CSV files from the CNES dataset (Elasticnes and API Dados Abertos SUS).
+"""Script to process the CSV files from the CNES dataset (Elasticnes and API
+Dados Abertos SUS).
 
-This scripts expects a `data/` folder where all CSV files are stored. The elasticnes Serviços Especializados must be located at `data/elasticnes.csv` 
-and the adasus responses must be located at `data/adasus/{cnes}.csv` for each CNES.
+This scripts expects a `data/` folder where all CSV files are stored. The
+elasticnes Serviços Especializados must be located at `data/elasticnes.csv` and
+the adasus responses must be located at `data/adasus/{cnes}.csv` for each CNES.
 """
 import pandas as pd
 import sys
@@ -11,9 +13,10 @@ from typing import TypedDict, Union
 from pprint import pprint
 import argparse
 
+
 class GeneralInfo(TypedDict):
     """GeneralInfo for a given CNES.
-    
+
     Args:
         cnes (int): CNES code
         name (str): Fantasy name of the establishment
@@ -37,7 +40,7 @@ class GeneralInfo(TypedDict):
     state: str
     kind: str
     cep: int
-    cnpj: int 
+    cnpj: int
     address: str
     number: str
     district: str
@@ -47,9 +50,10 @@ class GeneralInfo(TypedDict):
     email: str
     shift: str
 
+
 class ServiceRecord(TypedDict):
     """ServiceRecord for a given CNES.
-    
+
     Args:
         cnes (int): CNES code
         servico (str): Service provided by the establishment
@@ -59,8 +63,11 @@ class ServiceRecord(TypedDict):
     service: str
     classification: str
 
+
 def general_info(elasticnes: Union[pd.DataFrame, str], adasus: Union[dict, str]) -> GeneralInfo:
-    """Joins the data from a elasticnes dataset row and a adasus request to `/cnes/estabelecimentos/{cnes}`. Both `Series` must have the same `CNES`/`codigo_cnes`.
+    """Joins the data from a elasticnes dataset row and a adasus request to
+    `/cnes/estabelecimentos/{cnes}`. Both `Series` must have the same
+    `CNES`/`codigo_cnes`.
 
     Args:
         elasticnes (DataFrame | str): the full elasticnes dataset or the name of the `.csv` file
@@ -72,32 +79,35 @@ def general_info(elasticnes: Union[pd.DataFrame, str], adasus: Union[dict, str])
     if isinstance(adasus, str):
         adasus = json.load(open(adasus))
 
-    elasticnes: pd.Series = elasticnes[elasticnes['CNES'] == adasus['codigo_cnes']].iloc[0]
-        
+    elasticnes: pd.Series = elasticnes[elasticnes['CNES']
+                                       == adasus['codigo_cnes']].iloc[0]
+
     return GeneralInfo(
-        cnes = elasticnes['CNES'],
-        name = elasticnes['NOME FANTASIA'],
-        city = elasticnes['MUNICÍPIO'],
-        state = elasticnes['UF'],
-        kind = elasticnes['TIPO NOVO DO ESTABELECIMENTO'],
-        cep = adasus["codigo_cep_estabelecimento"],
-        cnpj = adasus["numero_cnpj"],
-        address = adasus["endereco_estabelecimento"],
-        number = adasus["numero_estabelecimento"],
-        district = adasus["bairro_estabelecimento"],
-        telephone = adasus["numero_telefone_estabelecimento"],
-        latitude = adasus["latitude_estabelecimento_decimo_grau"],
-        longitude = adasus["longitude_estabelecimento_decimo_grau"],
-        email = adasus["endereco_email_estabelecimento"],
-        shift = adasus["descricao_turno_atendimento"]
+        cnes=elasticnes['CNES'],
+        name=elasticnes['NOME FANTASIA'],
+        city=elasticnes['MUNICÍPIO'],
+        state=elasticnes['UF'],
+        kind=elasticnes['TIPO NOVO DO ESTABELECIMENTO'],
+        cep=adasus["codigo_cep_estabelecimento"],
+        cnpj=adasus["numero_cnpj"],
+        address=adasus["endereco_estabelecimento"],
+        number=adasus["numero_estabelecimento"],
+        district=adasus["bairro_estabelecimento"],
+        telephone=adasus["numero_telefone_estabelecimento"],
+        latitude=adasus["latitude_estabelecimento_decimo_grau"],
+        longitude=adasus["longitude_estabelecimento_decimo_grau"],
+        email=adasus["endereco_email_estabelecimento"],
+        shift=adasus["descricao_turno_atendimento"]
     )
 
-def service_records(elasticnes: Union[pd.DataFrame, str]) -> list[ServiceRecord]: 
-    """Filters useful service information from the elasticnes dataset and build a ServiceRecord list.
+
+def service_records(elasticnes: Union[pd.DataFrame, str]) -> list[ServiceRecord]:
+    """Filters useful service information from the elasticnes dataset and build
+    a ServiceRecord list.
 
     Args:
-        elasticnes (str): "Serviços Especializados" `.csv` file from elasticnes 
-    
+        elasticnes (str): "Serviços Especializados" `.csv` file from elasticnes
+
     Returns:
         list[ServiceRecord]: lista of ServiceRecord
     """
@@ -105,10 +115,10 @@ def service_records(elasticnes: Union[pd.DataFrame, str]) -> list[ServiceRecord]
         data = pd.read_csv(elasticnes)
     else:
         data = elasticnes
-    
+
     services_table = data[['CNES', 'SERVIÇO', 'SERVIÇO CLASSIFICAÇÃO']]
     services_table.columns = ['cnes', 'service', 'classification']
     services_list = services_table.to_dict(orient='records')
     services_records = [ServiceRecord(**service) for service in services_list]
-    
+
     return services_records
