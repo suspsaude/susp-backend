b0fbd93eb7a4cf830f65a990598b3b2f747199f9
Thiago Duvanel <thiago.duvanel.ferreira@live.com>
2024-09-26 14:39:46 -0300

    fix: table types were incorrect

--- src/db/tables.py
+++ src/db/tables.py
@@ -1,70 +1,71 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
-Base = declarative_base()
+Base=declarative_base()
 
 class GeneralInfo(Base):
     """GeneralInfo for a given CNES.
     
     Args:
-        cnes (int): CNES code
-        name (str): Fantasy name of the establishment
-        city (str): City where the establishment is located - Optional (filters to SP)
-        state (str): State where the establishment is located - Optional (filters to SP)
-        kind (str): Kind of establishment (TIPO NOVO DO ESTABELECIMENTO)
-        cep (int): CEP of the establishment
-        cnpj (int): CNPJ of the establishment
-        address (str): Address of the establishment
-        number (str): Address number of the establishment
-        district (str): District of the establishment
-        telephone (str): Telephone of the establishment
-        latitude (float): Latitude of the establishment (latitude_estabelecimento_decimo_grau)
-        longitude (float): Longitude of the establishment (longitude_estabelecimento_decimo_grau)
-        email (str): e-mail address of the establishment
-        shift (str): Establishment shift (descricao_turno_atendimento)
+        cnes (Integer): CNES code
+        name (String): Fantasy name of the establishment
+        city (String): City where the establishment is located - Optional (filters to SP)
+        state (String): State where the establishment is located - Optional (filters to SP)
+        kind (String): Kind of establishment (TIPO NOVO DO ESTABELECIMENTO)
+        cep (Integer): CEP of the establishment
+        cnpj (Integer): CNPJ of the establishment
+        address (String): Address of the establishment
+        number (String): Address number of the establishment
+        district (String): District of the establishment
+        telephone (String): Telephone of the establishment
+        latitude (Float): Latitude of the establishment (latitude_estabelecimento_decimo_grau)
+        longitude (Float): Longitude of the establishment (longitude_estabelecimento_decimo_grau)
+        email (String): e-mail address of the establishment
+        shift (String): Establishment shift (descricao_turno_atendimento)
     """
-    _tablename_ = 'general_infos'
+    __tablename__='general_infos'
 
-    cnes: Column(int, primary_key=True)
-    name: Column(str)
-    # city: Column(str)
-    # state: Column(str)
-    kind: Column(str)
-    cep: Column(int)
-    cnpj: Column(int) 
-    address: Column(str)
-    number: Column(str)
-    district: Column(str)
-    telephone: Column(str)
-    latitude: Column(float)
-    longitude: Column(float)
-    email: Column(str)
-    shift: Column(str)
+    cnes=Column(Integer, primary_key=True)
+    name=Column(String)
+    city=Column(String)
+    state=Column(String)
+    kind=Column(String)
+    cep=Column(Integer)
+    cnpj=Column(Integer) 
+    address=Column(String)
+    number=Column(String)
+    district=Column(String)
+    telephone=Column(String)
+    latitude=Column(Float)
+    longitude=Column(Float)
+    email=Column(String)
+    shift=Column(String)
     
 class MedicalService(Base):
     """MedicalService for a type of service.
 
     Args:
-        id (int): Medical service id
-        name (str): Description of the medical expertise
+        id (Integer): Medical service id
+        name (String): Description of the medical expertise
     """
-    __tablename__ = 'medical_services'
+    __tablename__='medical_services'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String)
+    id=Column(Integer, primary_key=True, autoincrement=True)
+    name=Column(String)
 
 class ServiceRecord(Base):
     """ServiceRecord for a given CNES.
     
     Args:
-        cnes (int): CNES code
-        service (int): ID of the service provided by the establishment (foreign key to MedicalService)
-        classification (str): Classification of the service
+        cnes (Integer): CNES code
+        service (Integer): ID of the service provided by the establishment (foreign key to MedicalService)
+        classification (String): Classification of the service
     """
-    __tablename__ = 'service_records'
+    __tablename__='service_records'
     
-    cnes = Column(Integer, primary_key=True)
-    service = Column(Integer, ForeignKey('medical_services.id'))
-    classification = Column(String)
+    cnes=Column(Integer, primary_key=True)
+    service=Column(Integer, ForeignKey('medical_services.id'))
+    classification=Column(String)
     
-    medical_service = relationship("MedicalService")
+    medical_service=relationship("MedicalService")
